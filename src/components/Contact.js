161be4fdb1ce4cff646ff519522230fc97870028
jflipp31698 <48108288+jflipp31698@users.noreply.github.com>
2020-12-01 22:40:05 -0600

    Added Chat, Calendar and Contact pages

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import React from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact Us</h1>
+            </div>
+        )
+    }
+}
+
+export default Contact;
